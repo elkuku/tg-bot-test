13f21d733b9da5a7052bf9699631da47df6786e8
elkuku <der.el.kuku@gmail.com>
2020-07-13 09:00:38 -0500

    Update

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,15 +3,37 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use TelegramBot\Api\BotApi;
+use TelegramBot\Api\Exception;
 
 class DefaultController extends AbstractController
 {
     /**
      * @Route("/", name="default")
      */
-    public function index()
+    public function index(): Response
     {
         return $this->render('default/index.html.twig');
+    }
+    /**
+     * @Route("/booo", name="booo")
+     */
+    public function boooindex(BotApi $botApi): Response
+    {
+
+        $chatId = -307725225;
+        $message = 'hello =;)';
+
+        try {
+            $result = $botApi->sendMessage($chatId, $message);
+            $text = 'Message has been sent to '.$result->getChat()->getTitle();
+            $this->addFlash('success', $text);
+        } catch (Exception $e) {
+            $this->addFlash('danger', $e->getMessage());
+        }
+
+        return $this->redirectToRoute('default');
     }
 }
